d18de47c5c18035dd66c2549b1a4ebdc32c03b0c
rubenscheedler <rubenscheedler@gmail.com>
2016-04-25 11:38:54 +0200

    added a generic class

---
+++ ExtendedVisioAddin1/ExtendedVisioAddin1/RationallyComponent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Office.Interop.Visio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExtendedVisioAddin1
+{
+
+
+    class RationallyComponent
+    {
+        private Master componentMaster;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="componentMaster">Component that is represented by this class.</param>
+        public RationallyComponent(Master componentMaster)
+        {
+            this.componentMaster = componentMaster;
+        }
+
+        public void Place()
+        {
+
+        }
+    }
+}

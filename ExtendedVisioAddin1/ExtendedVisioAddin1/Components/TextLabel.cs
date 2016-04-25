7178db4085afd1bd4c5089a554f9e9d682bfb494
rubenscheedler <rubenscheedler@gmail.com>
2016-04-25 12:45:29 +0200

    started on header label

--- ExtendedVisioAddin1/ExtendedVisioAddin1/Components/TextLabel.cs
+++ ExtendedVisioAddin1/ExtendedVisioAddin1/Components/TextLabel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Office.Interop.Visio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,9 +8,23 @@ namespace ExtendedVisioAddin1.Components
 {
     class TextLabel : RationallyComponent
     {
+        private String text;
+
         public TextLabel(String text)
         {
+            this.text = text;
+            
 
+        }
+
+        public void Draw(double x, double y)
+        {
+            Shape textShape = Globals.ThisAddIn.Application.ActivePage.DrawRectangle(x, y, x+text.Length*0.3, y+30);
+            //headerShape.TextStyle = "Basic";
+            textShape.LineStyle = "Text Only";
+            textShape.FillStyle = "Text Only";
+            textShape.Characters.Text = text;
+            textShape.Characters.CharProps[(short)VisCellIndices.visCharacterSize] = 22;
         }
     }
 }

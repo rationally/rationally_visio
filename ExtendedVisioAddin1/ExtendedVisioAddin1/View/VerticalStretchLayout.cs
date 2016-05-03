52c307da51ef39a15e85c71a55f8f7d567cfdd85
rubenscheedler <rubenscheedler@gmail.com>
2016-05-03 15:37:07 +0200

    started on verticalstretch layout manager

---
+++ ExtendedVisioAddin1/ExtendedVisioAddin1/View/VerticalStretchLayout.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExtendedVisioAddin1.View
+{
+    class VerticalStretchLayout : ILayoutManager
+    {
+        private RContainer toManage;
+
+        public VerticalStretchLayout(RContainer toManage)
+        {
+            this.toManage = toManage;
+        }
+
+        public void Draw(int x, int y)
+        {
+            
+        }
+
+        public void Repaint()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

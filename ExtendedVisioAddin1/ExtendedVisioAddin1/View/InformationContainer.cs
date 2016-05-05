16516b56a5fdfa9202e4f35305b623b3f9e5441a
rubenscheedler <rubenscheedler@gmail.com>
2016-05-05 15:18:15 +0200

    fixed decision title and decision information (they now use our components and layoutmanagers)

---
+++ ExtendedVisioAddin1/ExtendedVisioAddin1/View/InformationContainer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ExtendedVisioAddin1.Components;
+using Microsoft.Office.Interop.Visio;
+
+namespace ExtendedVisioAddin1.View
+{
+    class InformationContainer : HeaderlessContainer
+    {
+        public InformationContainer(Page page, string author, string date, string version) : base(page)
+        {
+            this.Width = 7;
+            this.Height = 1;
+            this.UsedSizingPolicy = SizingPolicy.ExpandXIfNeeded; //TODO fix with expandY
+
+            TextLabel authorLabel = new TextLabel(page, "Author: " + author);
+            authorLabel.SetMargin(0.1);
+            TextLabel dateLabel = new TextLabel(page, "Date: " + date);
+            dateLabel.SetMargin(0.1);
+            TextLabel versionLabel = new TextLabel(page, "Version: " + version);
+            versionLabel.SetMargin(0.1);
+
+            this.Children.Add(authorLabel);
+            this.Children.Add(dateLabel);
+            this.Children.Add(versionLabel);
+        }
+    }
+}

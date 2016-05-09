2a87569c7e5f1ad209ceff850c078f040d737ec9
R. Kruizinga <r.m.kruizinga@student.rug.nl>
2016-05-09 14:28:31 +0200

    Fixed firsts

--- ExtendedVisioAddin1/ExtendedVisioAddin1/View/RView.cs
+++ ExtendedVisioAddin1/ExtendedVisioAddin1/View/RView.cs
@@ -27,7 +27,7 @@ namespace ExtendedVisioAddin1.View
         public void AddAlternative(Alternative alternative)
         {
 
-            AlternativesContainer alternativesContainer = (AlternativesContainer)Children.First(c => c is AlternativesContainer);
+            AlternativesContainer alternativesContainer = (AlternativesContainer)Children.First(c => c is AlternativesContainer); //todo: We don't know how many
             ((RContainer)Globals.ThisAddIn.View.Children.First(ch => ch is AlternativesContainer)).Children.Add(new AlternativeContainer(Globals.ThisAddIn.Application.ActivePage, Globals.ThisAddIn.model.Alternatives.Count - 1, alternative));
             new RepaintHandler();
         }
@@ -38,7 +38,8 @@ namespace ExtendedVisioAddin1.View
         /// <param name="index">identifier of the alternative.</param>
         public void DeleteAlternative(int index)
         {
-            AlternativesContainer alternativesContainer = (AlternativesContainer)Children.First(c => c is AlternativesContainer);
+            AlternativesContainer alternativesContainer = (AlternativesContainer)Children.First(c => c is AlternativesContainer); //todo: we still don't know how many
+
             AlternativeContainer alternative = (AlternativeContainer) alternativesContainer.Children.First(x => x.AlternativeIndex == index && x is AlternativeContainer);
             alternativesContainer.Children.Remove(alternative);
             alternative.RShape.DeleteEx(0); //deletes the alternative, and it's child components

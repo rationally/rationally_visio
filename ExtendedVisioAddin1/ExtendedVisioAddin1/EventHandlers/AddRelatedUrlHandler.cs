01a7714da9b1d55ff821c337b42ebd7b5211b30d
R. Kruizinga <r.m.kruizinga@student.rug.nl>
2016-05-09 19:24:04 +0200

    url related stuffs

---
+++ ExtendedVisioAddin1/ExtendedVisioAddin1/EventHandlers/AddRelatedUrlHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExtendedVisioAddin1.EventHandlers
+{
+    internal class AddRelatedUrlHandler
+    {
+        //TODO: popUp and validation? etc
+    }
+}
---
+++ ExtendedVisioAddin1/ExtendedVisioAddin1/View/Documents/RelatedURLComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Interop.Visio;
+
+namespace ExtendedVisioAddin1.View.Documents
+{
+    internal class RelatedUrlComponent : RComponent
+    {
+        public RelatedUrlComponent(Page page, string url) : base(page)
+        {
+
+        }
+    }
+}

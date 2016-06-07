1d77dbbe7ac6d0e2a1c58e8b063faae1454aae14
rubenscheedler <rubenscheedler@gmail.com>
2016-06-07 12:44:33 +0200

    started on moveup/down forces

---
+++ ExtendedVisioAddin1/ExtendedVisioAddin1/EventHandlers/MoveDownForceHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ExtendedVisioAddin1.Model;
+using Microsoft.Office.Interop.Visio;
+
+namespace ExtendedVisioAddin1.EventHandlers
+{
+    class MoveDownForceHandler : MarkerEventHandler
+    {
+        public override void Execute(RModel model, Shape changedShape, string identifier)
+        {
+
+        }
+    }
+}
---
+++ ExtendedVisioAddin1/ExtendedVisioAddin1/EventHandlers/MoveUpForceHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ExtendedVisioAddin1.Model;
+using Microsoft.Office.Interop.Visio;
+
+namespace ExtendedVisioAddin1.EventHandlers
+{
+    internal class MoveUpForceHandler : MarkerEventHandler
+    {
+        public override void Execute(RModel model, Shape changedShape, string identifier)
+        {
+
+        }
+    }
+}

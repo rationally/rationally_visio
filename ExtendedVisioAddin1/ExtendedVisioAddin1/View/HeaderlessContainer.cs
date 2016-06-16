d9cec57e443b987301a56315c941158231b753bb
R. Kruizinga <r.m.kruizinga@student.rug.nl>
2016-06-16 14:32:32 +0200

    aiaiaia

--- ExtendedVisioAddin1/ExtendedVisioAddin1/View/HeaderlessContainer.cs
+++ ExtendedVisioAddin1/ExtendedVisioAddin1/View/HeaderlessContainer.cs
@@ -6,12 +6,13 @@ namespace ExtendedVisioAddin1.View
     {
         public HeaderlessContainer(Page page) : base(page)
         {
+
             Application application = Globals.ThisAddIn.Application;
             Document containerDocument = application.Documents.OpenEx(application.GetBuiltInStencilFile(VisBuiltInStencilTypes.visBuiltInStencilContainers, VisMeasurementSystem.visMSUS), (short)VisOpenSaveArgs.visOpenHidden);
             Master containerMaster = containerDocument.Masters["Plain"];
 
             RShape = Page.DropContainer(containerMaster, null);
-            
+
             RShape.CellsU["User.msvSDHeadingStyle"].ResultIU = 0; //Remove header
             containerDocument.Close();
         }

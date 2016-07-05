d7206da0828854f5f8aafabcfe58e41e54ebd379
R. Kruizinga <r.m.kruizinga@student.rug.nl>
2016-07-05 15:08:49 +0200

    blup

--- ExtendedVisioAddin1/ExtendedVisioAddin1/View/Alternatives/AlternativeContainer.cs
+++ ExtendedVisioAddin1/ExtendedVisioAddin1/View/Alternatives/AlternativeContainer.cs
@@ -199,24 +199,24 @@ namespace ExtendedVisioAddin1.View.Alternatives
             {
                 UpdateReorderFunctions();
             }
-            if (!(Children[0] is AlternativeIdentifierComponent))
-            {
-                RComponent c = Children.Find(x => x is AlternativeIdentifierComponent);
-                Children.Remove(c);
-                Children.Insert(0, c);
-            }
-            if (!(Children[1] is AlternativeTitleComponent))
-            {
-                RComponent c = Children.Find(x => x is AlternativeTitleComponent);
-                Children.Remove(c);
-                Children.Insert(1, c);
-            }
-            if (!(Children[2] is AlternativeStateComponent))
-            {
-                RComponent c = Children.Find(x => x is AlternativeStateComponent);
-                Children.Remove(c);
-                Children.Insert(2, c);
-            }
+                if (!(Children[0] is AlternativeIdentifierComponent))
+                {
+                    RComponent c = Children.Find(x => x is AlternativeIdentifierComponent);
+                    Children.Remove(c);
+                    Children.Insert(0, c);
+                }
+                if (!(Children[1] is AlternativeTitleComponent))
+                {
+                    RComponent c = Children.Find(x => x is AlternativeTitleComponent);
+                    Children.Remove(c);
+                    Children.Insert(1, c);
+                }
+                if (!(Children[2] is AlternativeStateComponent))
+                {
+                    RComponent c = Children.Find(x => x is AlternativeStateComponent);
+                    Children.Remove(c);
+                    Children.Insert(2, c);
+                }
             base.Repaint();
         }
     }

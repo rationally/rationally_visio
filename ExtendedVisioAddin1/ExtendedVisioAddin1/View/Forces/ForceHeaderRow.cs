e5fc03306eb4759b5178ee38d31e21a835c649b0
R. Kruizinga <r.m.kruizinga@student.rug.nl>
2016-07-07 14:53:07 +0200

    Locks

--- ExtendedVisioAddin1/ExtendedVisioAddin1/View/Forces/ForceHeaderRow.cs
+++ ExtendedVisioAddin1/ExtendedVisioAddin1/View/Forces/ForceHeaderRow.cs
@@ -18,6 +18,7 @@ namespace ExtendedVisioAddin1.View.Forces
             RationallyType = "forceHeaderRow";
             Name = "ForceHeaderRow";
 
+            MsvSdContainerLocked = true;
             InitChildren(page);
             Height = 0.33;
             InitStyle();

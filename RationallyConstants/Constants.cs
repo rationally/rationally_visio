a3d7076d1a5752a1afe1a063c6e0ec9a0ff57de8
R. Kruizinga <r.m.kruizinga@student.rug.nl>
2016-11-09 11:48:25 +0100

    changed constant

--- RationallyConstants/Constants.cs
+++ RationallyConstants/Constants.cs
@@ -13,7 +13,7 @@ namespace Rationally.Visio.RationallyConstants
 #if DEBUG
         public static readonly string MyShapesFolder = System.IO.Path.GetFullPath(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\Resources"));//<---- test path
 #else
-        public static readonly string MyShapesFolder = Environment.GetFolderPath(Environment.Is64BitOperatingSystem ? Environment.SpecialFolder.ProgramFilesX86 : Environment.SpecialFolder.ProgramFiles) + @"\rationally-visio\"; //<--- Enable for working add in
+        public static readonly string MyShapesFolder = Environment.GetFolderPath(Environment.Is64BitOperatingSystem ? Environment.SpecialFolder.ProgramFilesX86 : Environment.SpecialFolder.ProgramFiles) + @"\rationally-visio"; //<--- Enable for working add in
 #endif
         public const int LeftAlignment = 0; //Visio's own enum is wrong
         public const int SupportedAmountOfAlternatives = 3;

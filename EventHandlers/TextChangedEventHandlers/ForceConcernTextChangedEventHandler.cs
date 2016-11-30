dbd638d34d2ab6b9eef3862c3ff7685246df1db9
R. Kruizinga <r.m.kruizinga@student.rug.nl>
2016-11-30 12:38:55 +0100

    Textchanged events for forces

---
+++ EventHandlers/TextChangedEventHandlers/ForceConcernTextChangedEventHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using Rationally.Visio.View;
+using Rationally.Visio.View.Forces;
+using Microsoft.Office.Interop.Visio;
+
+namespace Rationally.Visio.EventHandlers.TextChangedEventHandlers
+{
+    internal class ForceConcernTextChangedEventHandler : ITextChangedEventHandler
+    {
+        public void Execute(RationallyView view, Shape changedShape)
+        {
+            ForceConcernComponent forceConcern = (ForceConcernComponent)view.GetComponentByShape(changedShape);
+            Globals.RationallyAddIn.Model.Forces[forceConcern.ForceIndex].Concern = forceConcern.Text;
+        }
+    }
+}
--- View/Forces/ForceValueComponent.cs
+++ View/Forces/ForceValueComponent.cs
@@ -31,7 +31,14 @@ namespace Rationally.Visio.View.Forces
             AddAction("addForce", "QUEUEMARKEREVENT(\"add\")", "\"Add force\"", false);
             AddAction("deleteForce", "QUEUEMARKEREVENT(\"delete\")", "\"Delete this force\"", false);
             AlternativeUniqueIdentifier = alternativeUniqueIdentifier;
-            Globals.RationallyAddIn.Model.Forces.ForEach(force => force.ForceValueDictionary.Add(alternativeUniqueIdentifier, "0"));
+            Globals.RationallyAddIn.Model.Forces.ForEach(force =>
+            {
+                if (!force.ForceValueDictionary.ContainsKey(alternativeUniqueIdentifier))
+                {
+                    force.ForceValueDictionary.Add(alternativeUniqueIdentifier, "0");
+                }
+            });
+
             InitStyle();
         }
 

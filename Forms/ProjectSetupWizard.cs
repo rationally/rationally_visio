ca828cbf4c83031b34518762caaa2c9150f60dab
rubenscheedler <rubenscheedler@gmail.com>
2016-12-06 15:06:55 +0100

    repared singeton pattern

--- Forms/ProjectSetupWizard.cs
+++ Forms/ProjectSetupWizard.cs
@@ -18,7 +18,7 @@ namespace Rationally.Visio.Forms
         {
             get
             {
-                if (instance?.IsDisposed != true)
+                if (instance == null || instance.IsDisposed)//if (instance?.IsDisposed != true)
                 {
                     instance = new ProjectSetupWizard();
                 }

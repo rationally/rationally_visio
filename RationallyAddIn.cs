1efc5730e46b82fb3f4856959139a5f73cba52df
R. Kruizinga <r.m.kruizinga@student.rug.nl>
2017-03-05 14:47:30 +0100

    Increased version number

--- RationallyAddIn.cs
+++ RationallyAddIn.cs
@@ -49,7 +49,7 @@ namespace Rationally.Visio
         public bool NewVersionAvailable;
 
         //Version numbers
-        internal readonly Version AddInLocalVersion = new Version("0.1.4");
+        internal readonly Version AddInLocalVersion = new Version("0.1.5");
         private Version addInOnlineVersion;
 
         private void RationallyAddIn_Startup(object sender, EventArgs e)
